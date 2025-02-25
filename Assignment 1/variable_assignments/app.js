6dd78c86c17f567aa008bf87317fc26ff1c7257d
Syed Muhammad Mubashir <syedmubikhan@gmail.com>
2025-02-26 01:17:33 +0500

    Add files via upload

---
+++ Assignment 1/variable_assignments/app.js
@@ -0,0 +1,34 @@
+// var myName = "Syed Muhammad Mubashir";
+// var age = "24 years old";
+// var institute = "SMIT";
+// var courseName = "Web and Mobile App Development"
+// var message = "Hello World";
+// alert(message);
+// alert(myName);
+// alert(age);
+// alert(institute);
+// alert(courseName);
+
+// var word = "PIZZA";
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// var myName2 = prompt("Enter your Name")
+// var noOfChilds = prompt("Enter number of childs")
+// var partnerName = prompt("Enter partner name")
+// var loc = prompt('Enter any location')
+// var jobTitle = prompt('Enter job title')
+
+// alert("My Name is "+myName2 +" Married to "+ partnerName +" I Live in "+ loc +" and I am a"+ jobTitle+".");
+
+// var email = "abc@gmail.com";
+// alert("my email address is " + email + ".")
+
+// var book = `"A Smarter Way To Learn Javascript."`;
+// alert("I am trying to learn a Book " + book);
+
+// document.write(
+//     `<h1>Yay! I can write HTML content through Javascript.<h1/>`
+// );
+
+
+
---
+++ Assignment 1/variable_num assignments/app.js
@@ -0,0 +1,16 @@
+// var age = 24;
+// alert(`I am ${age} years old.`);
+
+// var visitCount = 24;
+// alert(`You have visited this site ${visitCount} times.`);
+
+// var birthYear = 2000;
+
+// document.write(
+    // `<h1>My Birth Year is ${birthYear}.</br>DataType of my variable is ${typeof(birthYear)}.<h1/>`
+// )
+
+// var visitorName = prompt("Enter your Name:")
+// var productName = prompt("Enter your Product:")
+// var quantity = prompt("Enter your Quantity:")
+// document.write(`${visitorName} Ordered ${quantity} ${productName} from XYZ Clothing Store.`)
